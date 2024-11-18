595ab5bf724a8100b0b1cf5d07c99f4d97ba747b
Niels <niels.poensgen@digitalcareerinstitute.org>
2024-11-18 11:14:53 +0100

    neue veränderung

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> !!!!!!!!!!
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+       hallo welt
       </p>
     </>
   )
